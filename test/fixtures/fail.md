7c45b078cc3b56a7c736652f50f10efdc9c50834
azu <azuciao@gmail.com>
2016-03-19 12:22:46 +0900

    chore(textlint): update textlint to 6
    It is depend on textlint >=5.6

--- test/fixtures/fail.md
+++
@@ -1,5 +0,0 @@
-# Test
-
-- [ ] TODO
-
-TODO: !!!!
--- test/no-todo-test.js
+++ test/no-todo-test.js
@@ -1,38 +1,66 @@
-// LICENSE : MIT
-"use strict";
-import assert from "power-assert";
-import fs from "fs";
-import path from "path";
-import {textlint} from "textlint";
-import noTodoRule from "../src/no-todo"
-// ✘ HTML Import
-// ✔ HTML Imports
-describe("spellcheck-tech-word.js", function () {
-    beforeEach(function () {
-        textlint.setupRules({
-            // rule-key : rule
-            "no-todo": noTodoRule
-        });
-    });
-    afterEach(function () {
-        textlint.resetRules();
-    });
-    context("when found TODO", function () {
-        it("should have error messages", function () {
-            var filePath = path.join(__dirname, "/fixtures/fail.md");
-            var result = textlint.lintFile(filePath);
-            assert(result.filePath === filePath);
-            assert(result.messages.length > 0);
-            var message = result.messages[0].message;
-            assert(message.indexOf("found TODO:") !== -1);
-        });
-    });
-    context("when not found TODO", function () {
-        it("should have not error messages", function () {
-            var filePath = path.join(__dirname, "/fixtures/pass.md");
-            var result = textlint.lintFile(filePath);
-            assert(result.filePath === filePath);
-            assert(result.messages.length == 0);
-        });
-    });
+const TextLintTester = require("textlint-tester");
+const tester = new TextLintTester();
+// rule
+import rule from "../src/no-todo";
+// ruleName, rule, { valid, invalid }
+tester.run("no-todo", rule, {
+    valid: [
+        // no match
+        "text",
+        // partial match
+        "TODOS:"
+    ],
+    invalid: [
+        // single match
+        {
+            text: "TODO: this is TODO",
+            errors: [
+                {
+                    message: "Found TODO: 'TODO: this is TODO'",
+                    line: 1,
+                    column: 1
+                }
+            ]
+        },
+        // multiple match in multiple lines
+        {
+            text: `TODO: this is TODO
+            
+- [ ] TODO`,
+            errors: [
+                {
+                    message: "Found TODO: 'TODO: this is TODO'",
+                    line: 1,
+                    column: 1
+                },
+                {
+                    message: "Found TODO: '- [ ] TODO'",
+                    line: 3,
+                    column: 3
+                }
+            ]
+        },
+        // multiple hit items in a line
+        {
+            text: "TODO: A TODO: B",
+            errors: [
+                {
+                    message: "Found TODO: 'TODO: A TODO: B'",
+                    line: 1,
+                    column: 1
+                }
+            ]
+        },
+        // exact match or empty
+        {
+            text: "THIS IS TODO:",
+            errors: [
+                {
+                    message: "Found TODO: 'TODO:'",
+                    line: 1,
+                    column: 9
+                }
+            ]
+        }
+    ]
 });
